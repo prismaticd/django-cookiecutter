7de32186c4502c20db9ba01df232fd01e07d5c0d
benoit <bchabord@gmail.com>
2017-06-05 15:57:26 +1200

    Update css framework

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/static_src/js/base.js
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/static_src/js/base.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-
+    var burger = document.querySelector('.nav-toggle');
+    var menu = document.querySelector('.nav-menu');
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
 });
