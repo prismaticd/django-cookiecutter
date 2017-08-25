1dbbbd6665097d2ae4affa77d3b09b5fcab65c2e
benoit <bchabord@gmail.com>
2017-08-26 05:04:54 +1200

    Remove allauth

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/base_settings.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/base_settings.py
@@ -174,13 +174,6 @@ LOGGING = {
     },
 }
 
-AUTHENTICATION_BACKENDS = (
-    # Needed to login by username in Django admin, regardless of `allauth`
-    "django.contrib.auth.backends.ModelBackend",
-    # `allauth` specific authentication methods, such as login by e-mail
-    "allauth.account.auth_backends.AuthenticationBackend"
-)
-
 SITE_ID = 1
 
 STATIC_ROOT = os.path.join(BASE_DIR, '.static')
