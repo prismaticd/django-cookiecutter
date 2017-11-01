68f1b7b3a6615fa343ba97b7afa4d9c4402737e5
Ben Chab <benauf@gmail.com>
2017-11-02 10:40:40 +1300

    Add wagtail test

--- tests.sh
+++ tests.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-python tests.py
+rm -rf ./tmp/
+cookiecutter --no_input --output_dir="./tmp/" .
+cd tmp
+cd project_name
+chmod +x manage.py
+echo "SECRET_KEY = 'secretkeytest'" > project_name/settings/local.py
+pip install -r requirements_dev.txt --upgrade
+./manage.py migrate
+./manage.py init_data
+./manage.py test
+
+rm -rf ./tmp/
+cookiecutter --no_input --output_dir="./tmp/" --config-file=cookiecutter-with-wagtail.yml .
 cd tmp
 cd project_name
 chmod +x manage.py
