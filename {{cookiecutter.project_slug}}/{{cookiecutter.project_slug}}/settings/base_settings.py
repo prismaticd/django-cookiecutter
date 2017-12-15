76276cb70848fbd03bccd07c2044ac0d1248dffb
John Carter <john@therefromhere.org>
2017-12-15 15:05:18 +1300

    enable USE_TZ by default (#11)

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/base_settings.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/base_settings.py
@@ -157,10 +157,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Pacific/Auckland'
 USE_I18N = False
 USE_L10N = False
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
