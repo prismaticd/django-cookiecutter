1083201467c2305966dd2c36e9d7b147ced891e2
John Carter <john.carter@prismaticdigital.com>
2018-08-06 15:01:09 +1200

    Fix recursive logging error wheni GrayFilter is in use and "django" logger is at DEBUG leve

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/contrib/customlogging.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/contrib/customlogging.py
@@ -1,12 +1,16 @@
-from .request import GLOBALS
-from django.conf import settings
 import logging
+
+from django.conf import settings
+
+from .request import GLOBALS
 
 
 class GrayFilter(logging.Filter):  # pragma: no cover
-
     def filter(self, record):
-        if hasattr(GLOBALS, "user") and GLOBALS.user:
+        # check for request._cached_user before accessing the user object,
+        # this means django.contrib.auth.middleware.get_user has returned.
+        # This avoid hitting a recursive loop when the "django" logger level = "DEBUG".
+        if hasattr(GLOBALS.request, "_cached_user") and hasattr(GLOBALS, "user") and GLOBALS.user:
             record.user_id = GLOBALS.user.id
 
         record.env = settings.STATIC_LOGGING.get("env")
